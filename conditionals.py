96e01e90a16c033d544713517d8408b094403b70
grafuj <sfrrki@gmail.com>
2024-11-25 12:40:16 -0800

    add morgage conditionals

--- conditionals.py
+++ conditionals.py
@@ -10,3 +10,17 @@ elif is_wet:
 else:
   print("It's a cold day")
   print("Wear warm clothes")
+
+
+
+price = 1000000
+good_credit = True
+down_payment = 0
+
+  if good_credit:
+    down_payment = 0.1* price
+  else:
+    down_payment = 0.2* price
+
+  print(f"Down payment: ${down_payment}")
+
